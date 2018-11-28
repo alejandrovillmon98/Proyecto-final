d607c19a6ef1b345bdd0310787961c65f6b0b23d
alejandrovillmon98 <alejandrovillmon@gmail.com>
2018-11-28 14:24:09 -0600

    Login

---
+++ Alve/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  Alve
+//
+//  Created by David Hdz on 11/28/18.
+//  Copyright © 2018 David Hdz. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func Entrar(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func Registrarse(_ sender: UIButton) {
+    }
+    
+}
